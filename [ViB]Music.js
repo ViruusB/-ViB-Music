0a703538ad75a0fda5ad6705760ccb8375e1d65e
ViruusB <59928103+ViruusB@users.noreply.github.com>
2021-03-06 17:16:42 +0100

    Update 2.0

--- [ViB]Music.js
+++ [ViB]Music.js
@@ -23,31 +23,17 @@ fs.readdir(__dirname + '/events/', (err, files) => {
   );
 });
 
-fs.readdir('./commandes/Info/', (err, files) => {
+fs.readdir('./commandes/', (err, files) => {
   if (err) return console.error(err);
   files.forEach((file) => {
     if (!file.endsWith('.js')) return;
-    let props = require(`./commandes/Info/${file}`);
+    let props = require(`./commandes/${file}`);
     let commandName = file.split('.')[0];
     client.commands.set(commandName, props);
     console.log(`Chargement de la Commande: ${commandName}`);
   });
   console.log(
-    `Nombre de commandes "Info" chargées ! Total: ${files.length}/${files.length}`
-  );
-});
-
-fs.readdir('./commandes/Music/', (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    if (!file.endsWith('.js')) return;
-    let props = require(`./commandes/Music/${file}`);
-    let commandName = file.split('.')[0];
-    client.commands.set(commandName, props);
-    console.log(`Chargement de la Commande: ${commandName}`);
-  });
-  console.log(
-    `Nombre de commandes "Music" chargées ! Total: ${files.length}/${files.length}`
+    `Nombre de commandes chargées ! Total: ${files.length}/${files.length}`
   );
 });
 
